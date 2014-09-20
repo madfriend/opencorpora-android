27bd6651541173205b5c73b47a15936dab30206d
Alex Chuchunkov <scarywound@gmail.com>
2014-09-20 18:17:14 +0400

    some network stuff

--- js/app.js
+++ js/app.js
@@ -1,11 +1,35 @@
 function init() {
 	document.addEventListener("deviceready", onDeviceReady, false);
+	document.addEventListener("online", onOnline, false);
+    document.addEventListener("offline", onOffline, false);
+}
+
+function onOnline() {
+	window.online = true;
+	$('.is-online').removeClass('hidden');
+}
+
+function onOffline() {
+	window.online = false;
+	$('.is-online').addClass('hidden');
 }
 
 function isLoggedIn() {
-	return true;
+	var token = window.localStorage.getItem("auth_token");
+	if (token) return true;
 }
 
 function onDeviceReady() {
-	if (isLoggedIn() && $('body').hasClass('index')) window.location = "tasks.html";
+	navigator.network.isReachable("opencorpora.org", isOnlineNow, {});
+
+	if (isLoggedIn() && $('body').hasClass('index'))
+		window.location = "tasks.html";
+}
+
+function isOnlineNow(reachability) {
+    // There is no consistency on the format of reachability
+    var networkState = reachability.code || reachability;
+
+    if (networkState == NetworkStatus.NOT_REACHABLE) return onOffline();
+    return onOnline();
 }
